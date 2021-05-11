9d421c1e63470d5688efe150afc3f17b420d61f3
Bauer Q <>
2021-05-11 16:23:20 +0000

    Прописали основные команды

--- handlers.py
+++ handlers.py
@@ -7,23 +7,23 @@ import time
 async def send_to_admin(dp):
   await bot.send_message(chat_is = admin_id, text = "Бот запущен!")
 
-@dp.message_handler(commamd=["start"])
+@dp.message_handler(commamds=["start"])
 async def start(message:Message):
   await message.answer("Работает")
 
-@dp.message_handler(commamd=["add"])
+@dp.message_handler(commamds=["add"])
 async def add(message:Message):
   await message.answer("Работает")
 
-@dp.message_handler(commamd=["show"])
+@dp.message_handler(commamds=["show"])
 async def show(message:Message):
   await message.answer("Работает")
 
-@dp.message_handler(commamd=["done"])
+@dp.message_handler(commamds=["done"])
 async def done(message:Message):
   await message.answer("Работает")
 
-@dp.message_handler(commamd=["help"])
+@dp.message_handler(commamds=["help"])
 async def help(message:Message):
   await message.answer(HELP)
 
