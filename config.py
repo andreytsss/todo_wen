726b0451eccdc2967cc9d52112cf4005c76f91a7
Bauer Q <>
2021-05-11 15:30:33 +0000

    Добавили файлы app.py, config.py, handlers.py. Прописали параметры бота в config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = "1701677398:AAGlHgy6dljbdhVHnWqWKLKRwveKkuEpug8"
+admin_id = 1620144939
+todo = {}
+HELP = """
+add      - Добавить задачу
+show     - Показать задачи
+done     - Убрать выполненную задачу
+help     - Вывести список команд
+"""
